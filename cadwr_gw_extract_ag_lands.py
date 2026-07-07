2fc811af4fc0b047e0e23fc092462e7b46df240f
Charles Morton <cgmorton@users.noreply.github.com>
2026-07-07 11:10:19 -0700

    Update export scripts to support hydrologic regions

--- cadwr_gw_extract_ag_lands.py
+++ cadwr_gw_extract_ag_lands.py
@@ -73,6 +73,8 @@ def main(
         feature_coll_id = 'projects/ee-cgmorton/assets/ca_counties'
         feature_id_property = 'NAME'
         feature_properties = ['GEOID', 'STATEFP', 'COUNTYFP']
+        # Example of other columns available in the county shapefile
+        #   that could be written as feature properties
         # ALAND: 9778891285
         # AWATER: 185818274
         # COUNTYFP: 089
@@ -85,6 +87,13 @@ def main(
         # STATEFP: 06
         # STATE_NAME: California
         # STUSPS: CA
+    elif features.lower() in ['regions', 'hr', 'hydrologic_regions']:
+        export_name = f'hydrologic_region_{export_name}'
+        feature_coll_id = 'projects/ee-cgmorton/assets/ca_hydrologic_regions'
+        # Feature property used to uniquely identify each feature
+        feature_id_property = 'HR_NAME'
+        # These feature properties will be written to the CSV files
+        feature_properties = ['HR_ID']
     else:
         raise ValueError(f'unsupported features parameter: {features}')
 
@@ -106,30 +115,12 @@ def main(
     if not os.path.isdir(export_ws):
         os.makedirs(export_ws)
 
+    # # DEADBEEF
+    # ee.Initialize(
+    #     ee.ServiceAccountCredentials('_', key_file='../../keys/openet-gee.json'),
+    #     opt_url='https://earthengine-highvolume.googleapis.com'
+    # )
     ee_initializer(project_id=project_id, opt_url='https://earthengine-highvolume.googleapis.com')
-
-    # # CIMIS Albers Equal Area Projection
-    # # Using the EPSG:3310 code wasn't working, so pulling wkt from a CIMIS image
-    # # Reduced the extent slightly from the default used for CIMIS
-    # export_crs = ee.Image('projects/openet/assets/meteorology/cimis/ancillary/mask').projection().wkt()
-    # export_extent = [-376010, -606000, 542010, 452010]
-    # cellsize = 30
-    # export_geo = [cellsize, 0, export_extent[0], 0, -cellsize, export_extent[3]]
-    # # CGM - Testing out other California export extents
-    # # export_extent = [-374000, -604300, 540340, 450320]
-    # # export_extent = [-373990-2000-20, -604000-2000, 540000+2000+10, 450000+2000+20]
-    # # export_crs = 'EPSG:3310'
-    # # export_extent = [-410000, -660010, 610000, 460010]
-    # # # UTM Zone 11
-    # # export_crs = 'EPSG:32611'
-    # # export_extent = [-134685, 3597465, 765315, 4677465]
-    # # # UTM Zone 10
-    # # export_crs = 'EPSG:32610'
-    # # export_extent = [374415, 3613515, 1319415, 4654515]
-    # # # WGS84
-    # # export_crs = 'EPSG:4326'
-    # # export_extent = [-124.5, 32.4, -114.0, 42.1]
-    # # cellsize = 0.000269494585235856472
 
     # Read the feature properties
     feature_info = {
@@ -164,7 +155,7 @@ def main(
         if not os.path.isdir(model_export_ws):
             os.makedirs(model_export_ws)
 
-        for image_date in sorted(date_list):
+        for image_date in sorted(date_list, reverse=reverse_flag):
             print(image_date.strftime("%Y-%m-%d"))
 
             model_date_csv = os.path.join(
@@ -182,10 +173,10 @@ def main(
                 for ftr_id, ftr_properties in feature_info.items()
             ]
 
-            # DEADBEEF - Test the function call for a single image and feature
+            # # Uncomment to test the function call for a single image and feature
             # print(feature_extract(
             #     image_date, model_coll_id, list(feature_info.keys())[0],
-            #     feature_coll_id, feature_id_property, et_band='et',
+            #     feature_coll_id, feature_id_property, et_band=et_band,
             # ))
             # break
 
@@ -216,6 +207,7 @@ def main(
 
 
 def ee_initializer(project_id='openet', opt_url='https://earthengine-highvolume.googleapis.com'):
+    # ee.Initialize(ee.ServiceAccountCredentials('_', key_file='../../keys/openet-gee.json'), opt_url=opt_url)
     ee.Initialize(project=project_id, opt_url=opt_url)
 
 
@@ -301,7 +293,7 @@ def arg_parse():
         description='Extract California/CIMIS OpenET monthly aggregations for agricultural lands',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '--features', default='counties', choices=['basins', 'counties'],
+        '--features', default='counties', choices=['basins', 'counties', 'regions'],
         help='Features to aggregate over')
     parser.add_argument(
         '--models', nargs='+', metavar='', default=MODELS, choices=MODELS,
